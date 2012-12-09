c9c08cc59d8d8562f8b631284cf5aa771969f1c4
Kris <Errett>
2012-12-09 15:41:03 -0500

    Revised stuff

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from google.appengine.api import urlfetch
+from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-from google.appengine.ext import db
+from google.appengine.api import urlfetch
 from google.appengine.dist import use_library
 use_library('django', '1.2')
 from google.appengine.ext.webapp import template
+from google.appengine.api import memcache
 import os
 from django.utils import simplejson as json # help -> http://stackoverflow.com/questions/1171584/how-can-i-parse-json-in-google-app-engine
 import string
